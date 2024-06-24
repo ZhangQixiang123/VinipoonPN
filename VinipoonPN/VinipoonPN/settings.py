0e70cd58e5446380c62a87e11e29f5e2bfbbad6a
ZhangQixiang123 <e1264831@u.nus.edu>
2024-06-24 15:39:07 +0800

    customized user

--- VinipoonPN/VinipoonPN/settings.py
+++ VinipoonPN/VinipoonPN/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+AUTH_USER_MODEL = 'accounts.CustomUser'
