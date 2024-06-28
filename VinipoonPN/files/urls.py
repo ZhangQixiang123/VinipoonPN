5bcb8068362cceceba21d7434a385284ea3f7a7b
ZhangQixiang123 <e1264831@u.nus.edu>
2024-06-28 23:11:34 +0800

    home added

--- VinipoonPN/files/urls.py
+++ VinipoonPN/files/urls.py
@@ -9,7 +9,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/', views.upload_file, name='upload_file'),
+    path('upload/', views.upload_file, name='home'),
     path('download/<int:file_id>/', views.download_file, name='download_file'),
     path('delete/<int:file_id>/', views.delete_file, name='delete_file'),
 ]
