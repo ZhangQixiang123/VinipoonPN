c8e7f64ff626cc15717e9996adc32d29fddc0985
ArtillerySun <e1297745@u.nus.edu>
2024-06-29 00:03:21 +0800

    modified:   backend/db.sqlite3 	modified:   lib/global.dart

--- lib/global.dart
+++ lib/global.dart
@@ -1,4 +1,6 @@
 library globals;
 
-const String server = 'http://127.0.0.1:8000';
+// const String server = 'http://207.148.64.177:8000';
 
+const String server = 'http://207.148.64.177:8000';
+// const String server = 'http://127.0.0.1:8000';
