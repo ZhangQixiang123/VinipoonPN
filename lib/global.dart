42a00801b0f563e1a41a4cf0cc8993df0a34b5d9
ArtillerySun <e1297745@u.nus.edu>
2024-06-29 20:14:23 +0800

    modified:   lib/global.dart

--- lib/global.dart
+++ lib/global.dart
@@ -1,6 +1,6 @@
 library globals;
 
-// const String server = 'http://207.148.64.177:8000';
+const String server = 'http://vinipoopn.xyz:8000';
 
-const String server = 'http://207.148.64.177:8000';
+// const String server = 'http://207.148.64.177:8000';
 // const String server = 'http://127.0.0.1:8000';
