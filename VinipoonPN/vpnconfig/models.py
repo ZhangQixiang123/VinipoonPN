dae89401b9759de64289b02396d3014ab34df6a6
ZhangQixiang123 <e1264831@u.nus.edu>
2024-07-27 15:29:02 +0800

    country code

--- VinipoonPN/vpnconfig/models.py
+++ VinipoonPN/vpnconfig/models.py
@@ -3,6 +3,7 @@ import uuid
 
 class VpnConfig(models.Model):
     name = models.CharField(max_length=200)
+    country_code = models.CharField(max_length=20)
     ip_address = models.GenericIPAddressField()
     port = models.IntegerField()
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
