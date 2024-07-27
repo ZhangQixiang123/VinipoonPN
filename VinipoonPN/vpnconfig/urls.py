5bb619d556b6a9e0bd4a2bada1cb157d919aebbc
ZhangQixiang123 <e1264831@u.nus.edu>
2024-07-27 12:13:57 +0800

    small modification

--- VinipoonPN/vpnconfig/urls.py
+++ VinipoonPN/vpnconfig/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import VpnConfigCreateView, VpnConfigListView, VpnConfigDetailView
 
 urlpatterns = [
-    path('api/vpn-config/', VpnConfigCreateView.as_view(), name='vpn-config-create'),
-    path('api/vpn-config/names/', VpnConfigListView.as_view(), name='vpn-config-names'),
-    path('api/vpn-config/<str:name>/', VpnConfigDetailView.as_view(), name='vpn-config-detail'),
+    path('api/', VpnConfigCreateView.as_view(), name='vpn-config-create'),
+    path('api/all/', VpnConfigListView.as_view(), name='vpn-config-names'),
+    path('api/<str:name>/', VpnConfigDetailView.as_view(), name='vpn-config-detail'),
 ]
